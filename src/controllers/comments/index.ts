0fabf5b2271b2e91c6ec731551c3ade5a37c7bdc
otumian-empire <popecan1000@gmail.com>
2023-09-02 22:29:46 +0000

    add endpoint and skeleton for comments

---
+++ src/controllers/comments/index.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const router = Router();
+
+// Comment end points
+
+export default router;
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,22 @@
 import { Router } from "express";
 
+import articles from "./articles";
+import comments from "./comments";
+import dashboard from "./dashboard";
+import users from "./users";
+
 const router = Router();
+
+// User
+router.use("/users", users);
+
+// Dashboard
+router.use("/dashboard", dashboard);
+
+// Articles
+router.use("/articles", articles);
+
+// Comments
+router.use("/comments", comments);
 
 export default router;
